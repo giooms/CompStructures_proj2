b04e397415af5937fcae64f13dcac5b68c645dbe
Gilles Ooms <gillesooms@outlook.com>
2024-12-05 06:39:50 +0100

    recursion ok but doesn't respect maxDepth

--- fractal.asm
+++ fractal.asm
@@ -7,57 +7,71 @@
 drawFractal:
     PUSH(LP) PUSH(BP)
     MOVE(SP, BP)
+    PUSH(R1) PUSH(R2) PUSH(R3) PUSH(R4)
 
-    PUSH(R1) PUSH(R2) PUSH(R3) PUSH(R4) PUSH(R5) PUSH(R6) PUSH(R7)
-
+    |; Load parameters
     LD(BP, -12, R1)         |; xTopLeft
     LD(BP, -16, R2)         |; yTopLeft
     LD(BP, -20, R3)         |; sideLength
     LD(BP, -24, R4)         |; maxDepth
 
-    CMPEQ(R4, 0, R5)        |; if (maxDepth == 0)
-    BT(R5, end_fractal)
+    |; Base case check
+    CMPEQ(R4, 0, R0)
+    BT(R0, end_fractal)
 
-    ANDC(R3, 1, R5)         |; if ((sideLength & 1) == 0)
-    BT(R5, skip_decrement)
-    SUBC(R3, 1, R3)         |; sideLength--
+    |; Handle even sideLength
+    ANDC(R3, 1, R0)
+    BT(R0, skip_decrement)
+    SUBC(R3, 1, R3)
 skip_decrement:
 
+    |; 1. Draw square first
     PUSH(R3) PUSH(R2) PUSH(R1)
-    CALL(drawSquare, 3)     |; Draw square first
+    CALL(drawSquare, 3)
 
-    DIVC(R3, 2, R5)         |; radius = sideLength / 2
-    ADD(R1, R5, R6)         |; xc = xTopLeft + radius
-    ADD(R2, R5, R7)         |; yc = yTopLeft + radius
+    |; 2. Draw circle inside square
+    |; Save ALL values including maxDepth
+    PUSH(R1) PUSH(R2) PUSH(R3) PUSH(R4)  |; Save ALL values including maxDepth
+    
+    |; Calculate circle parameters
+    DIVC(R3, 2, R0)         |; radius = sideLength/2
+    ADD(R1, R0, R1)         |; xc = xTopLeft + radius
+    ADD(R2, R0, R2)         |; yc = yTopLeft + radius
+    MOVE(R0, R3)            |; R3 = radius
+    
+    |; Draw circle
+    PUSH(R3) PUSH(R2) PUSH(R1)
+    CALL(drawCircleBres, 3)
+    MOVE(R0, R5)            |; Save lastPixelCircleX in R5
+    
+    |; 3. Calculate new square parameters
+    |; Restore ALL values
+    POP(R4) POP(R3) POP(R2) POP(R1)      |; Restore ALL values
+    DIVC(R3, 2, R0)          |; radius = sideLength/2
+    SUB(R0, R5, R0)          |; shift = radius - lastPixelCircleX
 
-    PUSH(R5) PUSH(R7) PUSH(R6)
-    CALL(drawCircleBres, 3) |; Draw circle and get lastPixelCircleX
-    MOVE(R0, R6)            |; Save lastPixelCircleX in R6
+    |; Check if should continue
+    CMPLTC(R0, 1, R5)
+    BT(R5, end_fractal)
 
-    SUB(R5, R6, R7)         |; shift = radius - lastPixelCircleX
-
-    CMPLTC(R7, 1, R0)       |; if (shift < 1)
-    BT(R0, end_fractal)     |; return if shift too small
-
-    |; Save original values before modifications
-    PUSH(R1) PUSH(R2) PUSH(R3) PUSH(R4)
-
-    |; Calculate new parameters
-    ADD(R1, R7, R1)         |; new xTopLeft = xTopLeft + shift
-    ADD(R2, R7, R2)         |; new yTopLeft = yTopLeft + shift
-    SHLC(R7, 1, R0)         |; 2 * shift
+    |; 4. Make recursive call with new parameters
+    PUSH(R0)                |; Save shift
+    ADD(R1, R0, R1)         |; new xTopLeft = xTopLeft + shift
+    ADD(R2, R0, R2)         |; new yTopLeft = yTopLeft + shift
+    POP(R0)                 |; Restore shift
+    SHLC(R0, 1, R0)         |; R0 = 2 * shift
     SUB(R3, R0, R3)         |; new sideLength = sideLength - (2 * shift)
-    SUBC(R4, 1, R4)         |; new maxDepth = maxDepth - 1
 
-    |; Make recursive call
+    |; After shift calculation
+    SUBC(R4, 1, R4)         |; Decrement maxDepth
+    |; Don't restore R4 from earlier save, since we want to keep the decremented value
+
+    |; Make recursive call with decremented maxDepth
     PUSH(R4) PUSH(R3) PUSH(R2) PUSH(R1)
     CALL(drawFractal, 4)
 
-    |; Restore original values
-    POP(R4) POP(R3) POP(R2) POP(R1)
-
 end_fractal:
-    POP(R7) POP(R6) POP(R5) POP(R4) POP(R3) POP(R2) POP(R1)
+    POP(R4) POP(R3) POP(R2) POP(R1)
     POP(BP) POP(LP)
     RTN()
 
